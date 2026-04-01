7622e4caa934397e1d4058b41192e61f3e6a65c8
angeladellarosaa <della250906@gmail.com>
2026-04-01 14:10:11 +0700

    remove suspense

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, Suspense} from 'react';
+import { useState, useEffect} from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import AuthFormWrapper from '../../../components/AuthFormWrapper';
 import SocialAuth from '../../../components/SocialAuth';
